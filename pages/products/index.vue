605c9330de48f9940724626fb777deeeae29ec5e
julianostroschon <julianostroschon@gmail.com>
2024-12-02 21:34:26 -0300

    🎨 Query de produtos sendo feita

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -6,23 +6,19 @@ const data = await GqlProductLoad()
   <div
     class="flex flex-col items-center justify-center w-full bg-slate-100 mx-10"
   >
-    <h1 class="text-3xl font-bold py-4">
+    <h1 class="text-1xl font-bold py-4">
       {{ $t('sidebar.products') }}
     </h1>
 
-    <div class="card rounded bg-slate-100 shadow-md w-full">
-      <!-- <div class="card-body">
-        <h2 class="card-title">
-          {{ $t('sidebar.products') }}
-        </h2>
-      </div> -->
-      <!-- {{ data.productLoad }} -->
+    <div class="card rounded bg-slate-200 shadow-md w-full">
+      {{ data.products }}
       <div
         id="products"
-        v-for="product in data.productLoad"
+        v-for="product in data.products"
         :key="product.id"
         class="w-full flex flex-col items-center justify-center p-4"
       >
+        oito
         <div class="card rounded bg-slate-500 shadow-md w-full p-4">
           <div class="card-body">
             <h2 class="card-title text-center">

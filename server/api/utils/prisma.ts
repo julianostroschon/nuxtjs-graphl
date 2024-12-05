575f47c3b8f839ca25db951c91f1a400c14c9dcc
julianostroschon <julianostroschon@gmail.com>
2024-12-05 11:47:24 -0300

    ✨ Corrige para um só cliente

--- server/api/utils/prisma.ts
+++ server/api/utils/prisma.ts
@@ -1,23 +1,21 @@
 import { PrismaClient } from '@prisma/client'
 
-const prismaClients: Record<string, PrismaClient> = {}
+// const prismaClients: Record<string, PrismaClient> = {}
 
-export function getPrismaClient(datasource: string): PrismaClient {
-  if (!prismaClients[datasource]) {
-    const formattedDatasource = datasource.replace(/-/g, '_')
-    const databaseUrl =
-      process.env[`DATABASE_URL_${formattedDatasource.toUpperCase()}`]
-    console.log('databaseUrl', databaseUrl)
-    if (!databaseUrl) {
-      throw new Error(`Database URL for datasource ${datasource} not found.`)
-    }
-    prismaClients[datasource] = new PrismaClient({
-      datasources: {
-        db: {
-          url: databaseUrl,
-        },
-      },
-    })
+export function getPrismaClient(datasource: string) {
+  // const formattedDatasource = datasource.replace(/-/g, '_')
+  const databaseUrl = process.env[`DATABASE_URL`]
+  // console.log('databaseUrl', databaseUrl)
+  if (!databaseUrl) {
+    throw new Error(`Database URL for datasource ${datasource} not found.`)
   }
-  return prismaClients[datasource]
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl,
+      },
+    },
+  })
+  // }
+  // return prismaClients[datasource]
 }
